4759d648b33c289f82cc536025ca772bd434a36b
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2023-09-27 19:01:09 -0400

    Add A6-7 solutions

--- src/HM/A6.hs
+++ src/HM/A6.hs
@@ -5,26 +5,43 @@ import HM.Provided
 
 -- Q#01
 
+type Chances    = Int
+type Guess      = String
+type Move       = Char
+type Secret     = String
+type Dictionary = [String]
+
 -- Q#02
-data GameException
+
+data GameException =  InvalidChars | InvalidLength | NotInDict | InvalidMove | RepeatMove | GameOver
 
 -- Q#03
 
-lengthInRange = undefined
+lengthInRange :: Secret -> Bool
+lengthInRange s = length s >= fst _LENGTH_ && length s <= snd _LENGTH_
 
 -- Q#04
 
-invalidMove = undefined
+invalidMove :: Move -> Bool
+invalidMove move = not (isAlpha move)
 
 -- Q#05
 
-revealLetters = undefined
+revealLetters :: Move -> Secret -> Guess -> Guess
+revealLetters move secret guess = zipWith (\s g -> if s == move then s else g) secret guess
 
 -- Q#06
 
-updateChances = undefined
+updateChances :: Move -> Secret -> Chances -> Chances
+updateChances move secret chances = if move `elem` secret then chances else chances - 1
 
 -- Q#07
 
-setSecret = undefined
-
+setSecret :: IO Secret
+setSecret = do
+  putStr "Enter a secret word:\t"
+  showInput False
+  secret <- getLine
+  showInput True
+  _SPACE_
+  return secret
--- src/HM/A7.hs
+++ src/HM/A7.hs
@@ -7,19 +7,40 @@ import System.Directory (doesFileExist)
 import Data.List (intersperse, sort)
 
 -- Q#01
-data Game
+
+data Game =
+  Game { getSecret  :: Secret
+       , getGuess   :: Guess
+       , getMoves   :: [Move]
+       , getChances :: Int
+       }
 
 -- Q#02
 
-repeatedMove = undefined
+repeatedMove :: Move -> Game -> Bool
+repeatedMove move game = move `elem` getMoves game
 
 -- Q#03
 
-makeGame = undefined
+makeGame :: Secret -> Game
+makeGame secret =
+  Game { getSecret  = map toUpper secret
+       , getGuess   = map (const '_') secret
+       , getMoves   = []
+       , getChances = _CHANCES_
+       }
 
 -- Q#04
 
-updateGame = undefined
+updateGame :: Move -> Game -> Game
+updateGame move game =
+  game { getGuess = newGuess
+       , getMoves = move : getMoves game
+       , getChances = newChances
+       }
+  where
+    newGuess   = revealLetters move (getSecret game) (getGuess game)
+    newChances = updateChances move (getSecret game) (getChances game)
 
 -- Q#05
 
@@ -33,31 +54,64 @@ showGameHelper game moves chances =
       _STARS_
     ]
 
+instance Show Game where
+  show :: Game -> String
+  show (Game _ g ms cs) = showGameHelper g ms cs
+
 -- Q#06
 
+instance Show GameException where
+  show :: GameException -> String
+  show InvalidChars  = "Word contains invalid characters: try again!"
+  show InvalidLength = concat ["Word must be between ", lb, " and ", ub, " characters: try again!"]
+    where
+      lb = show $ fst _LENGTH_
+      ub = show $ snd _LENGTH_
+  show NotInDict   = "Word is not in dictionary: try again!"
+  show InvalidMove = "Invalid character: try again!"
+  show RepeatMove  = "You guessed that already: try again!"
+  show GameOver    = "Sorry, game over!"
 
 -- Q#07
 
-toMaybe = undefined
+toMaybe :: Bool -> a -> Maybe a
+toMaybe b a = if b then Just a else Nothing
 
 -- Q#08
 
-validateSecret = undefined
+validateSecret :: (Secret -> Bool) -> GameException -> Secret -> Either GameException Secret
+validateSecret p e s = if p s then Right s else Left e
 
 -- Q#09
 
-hasValidChars = undefined
+hasValidChars :: Secret -> Either GameException Secret
+hasValidChars = validateSecret (all isAlpha) InvalidChars
 
-isValidLength = undefined
+isValidLength :: Secret -> Either GameException Secret
+isValidLength = validateSecret lengthInRange InvalidLength
 
-isInDict = undefined
+isInDict :: Dictionary -> Secret -> Either GameException Secret
+isInDict dict = validateSecret (\s -> map toLower s `elem` dict) NotInDict
 
 -- Q#10
 
-validateNoDict = undefined
+validateNoDict :: Secret -> Either GameException Secret
+validateNoDict s = case hasValidChars s of
+  Right s' -> isValidLength s'
+  err -> err
 
-validateWithDict = undefined
+validateWithDict :: Dictionary -> Secret -> Either GameException Secret
+validateWithDict dict s = case validateNoDict s of
+  Right s' -> isInDict dict s'
+  err -> err
 
 -- Q#11
 
-processTurn = undefined
+processTurn :: Move -> Game -> Either GameException Game
+processTurn move game
+  | invalidMove move        = Left InvalidMove
+  | repeatedMove move game  = Left RepeatMove
+  | getChances newGame == 0 = Left GameOver
+  | otherwise               = Right newGame
+  where
+    newGame = updateGame move game
