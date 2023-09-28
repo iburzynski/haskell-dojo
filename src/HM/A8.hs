b0ac791d22d71be6d2214abb1111794f42c455f6
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2023-09-27 22:52:39 -0400

    Add A8 solutions

--- src/HM/A8.hs
+++ src/HM/A8.hs
@@ -9,40 +9,80 @@ import System.Directory (doesFileExist)
 
 -- Q#01
 
-getUpperChar = undefined
+getUpperChar :: IO Char
+getUpperChar = toUpper <$> getChar
 
 -- Q#02
 
+_DICT_ :: IO Dictionary
 _DICT_ = do
-  fileExists <- doesFileExist _DICT_FILE_
-  if fileExists then undefined else undefined
+  exists <- doesFileExist _DICT_FILE_
+  if exists then words <$> readFile _DICT_FILE_ else pure []
 
 isDictNonEmpty :: IO Bool
-isDictNonEmpty = undefined
+isDictNonEmpty = not . null <$> _DICT_
 
 -- Q#03
 
-makeGameIfValid = undefined
+makeGameIfValid :: Either GameException Secret -> Either GameException Game
+makeGameIfValid secret = makeGame <$> secret
 
 -- Q#04
 
-getDict = undefined
+getDict :: IO (Maybe Dictionary)
+getDict = toMaybe <$> isDictNonEmpty <*> _DICT_
 
 -- Q#05
 
-validateNoDict = undefined
+validateNoDict :: Secret -> Either GameException Secret
+validateNoDict s = hasValidChars s >>= isValidLength
 
-validateWithDict = undefined
+validateWithDict :: Dictionary -> Secret -> Either GameException Secret
+validateWithDict dict s = validateNoDict s >>= isInDict dict
 
 -- Q#06
 
-playGame = undefined
+playGame :: Game -> IO ()
+playGame game = do
+  promptGuess
+  move <- getUpperChar
+  _SPACE_
+  case processTurn move game of
+    Left GameOver -> print GameOver >> putStrLn ("The word was: " ++ getSecret game)
+    Left e        -> print e >> playGame game
+    Right game'   -> do
+      print game'
+      if   getGuess game' == getSecret game'
+      then putStrLn "Yay, correct!"
+      else playGame game'
 
 -- Q#07
 
-startGame = undefined
+startGame :: (Secret -> Either GameException Secret) -> IO ()
+startGame validator = do
+  secret <- setSecret
+  case makeGameIfValid (validator secret) of
+    Left  e -> print e >> startGame validator
+    Right g -> print g >> playGame g
 
 -- Q#08
 
 runHM :: IO ()
-runHM = putStrLn "Not implemented... yet!"
+runHM = do
+  maybeDict <- getDict
+  case maybeDict of
+    Just dict -> startGame (validateWithDict dict)
+    Nothing   -> do
+      putStrLn "Missing dictionary! Continue without dictionary? [Y/N]"
+      c <- getUpperChar
+      when (c == 'Y') $ startGame validateNoDict
+
+runHM' :: IO ()
+runHM' = do
+  dict <- _DICT_
+  case dict of
+    []   -> do
+      putStrLn "Missing dictionary! Continue without dictionary? [Y/N]"
+      c <- getUpperChar
+      when (c == 'Y') $ startGame validateNoDict
+    words -> startGame (validateWithDict words)
