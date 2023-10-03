538f2960813d1c73a41c1592cc80ff456bf2b030
urapua katjizeu <vakaakatjizeu@Gmail.com>
2023-10-03 13:30:11 +0000

    Session 2 Day 1 Haskell session

---
+++ Testing/D2.hs
@@ -0,0 +1,23 @@
+-- double x = x+x
+
+-- add x y = x+y
+-- identity x = x --identity double ( (add 1 2))
+
+x :: Int -- signature
+x = 2
+
+z :: Bool
+z = True
+
+a :: ()
+a = ()
+
+isEven :: Int -> Bool
+isEven x = mod x 2 == 0 -- tells me if a number (input ) is even[True] or not[False]
+
+--za :: [Char]
+-- za = ['H','a','s','k','e','l','l']
+za :: String
+za = 'Haskell'
+
+
