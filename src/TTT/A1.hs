ecf891c96bfe0a316546035cfec4a34e21c351bd
urapua katjizeu <vakaakatjizeu@Gmail.com>
2023-10-05 08:55:31 +0000

    A1

--- src/TTT/A1.hs
+++ src/TTT/A1.hs
@@ -3,12 +3,12 @@ module TTT.A1 where
 import Data.Char (toUpper)
 
 -- Q#01
-
-_SIZE_ = undefined
+_SIZE_ :: Int
+_SIZE_ = 3
 
 -- Q#02
-
-_DISPLAY_LOGO_ = undefined
+_DISPLAY_LOGO_ :: Bool
+_DISPLAY_LOGO_ = True
 
 -- Q#03
 
